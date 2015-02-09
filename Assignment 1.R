350c081cd7b2ed6911c7ee5c3d887cf555d32273
Yasin Hatiboğlu <spyahsin@hotmail.com>
2015-02-09 02:00:01 +0200

    first submission

---
+++ Assignment 1.R
@@ -0,0 +1,57 @@
+library(sqldf)
+library(lubridate)
+
+setwd("../../../../Coursera/")
+
+data <- read.csv.sql("exdata-data-household_power_consumption/household_power_consumption.txt", header=TRUE, sep=";",
+                     sql="Select * from file where Date = '1/2/2007' OR Date = '2/2/2007'")
+
+
+
+Date<-data[,1]
+Time<-data[,2]
+
+
+DateTime<-strptime(paste(data=Date,Time), "%d/%m/%Y %H:%M:%S")
+DateTime<-as.POSIXct(DateTime)
+
+#### Plot 1 ####
+png("plot1.png",width = 480,height = 480)
+hist(data$Global_active_power,col="red",main="Global Active Power",xlab="Global Active Power (kilowatts)")
+dev.off()
+
+
+#### Plot 2 ####
+# First change local settings
+Sys.setlocale("LC_TIME", "English")
+png("plot2.png",width = 480,height = 480)
+plot(x=DateTime,y=data$Global_active_power,type="l",ylab="Global Active Power (kilowatts)",xlab="")
+dev.off()
+
+#### Plot 3 ####
+png("plot3.png",width = 480,height = 480)
+plot(x=DateTime,data[,7],type="l",ylab="Energy sub metering",xlab="")
+lines(x=DateTime,y=data[,8],col="red")
+lines(x=DateTime,data[,9],col="blue")
+legend("topright",lty=1,  col=1:3,  c("sub_metering_1","sub_metering_2","sub_metering_3"))
+dev.off()
+
+#### Plot 4 ####
+png("plot4.png",width = 480,height = 480)
+par(mfrow=c(2,2))
+
+#1
+plot(x=DateTime,data$Global_active_power,type="l",ylab="Global Active Power",xlab="")
+
+#2
+plot(x=DateTime,data[,5],type="l",ylab="Voltage",xlab="")
+
+#3
+plot(x=DateTime,data[,7],type="l",ylab="Energy sub metering",xlab="")
+lines(x=DateTime,data[,8],col="red")
+lines(x=DateTime,data[,9],col="blue")
+legend("topright",lty=1,  col=1:3,  c("sub_metering_1","sub_metering_2","sub_metering_3"),bty = "n")
+
+#4
+plot(x=DateTime,data[,4],type="l",ylab="Global Reactive Power",xlab="")
+dev.off()
